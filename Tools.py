5b1fd6bec865e3b03f510a128bb650e1f0db5439
Mario Alessandro Napoli <mario@creationdose.com>
2021-08-07 22:14:51 +0200

    Update Refactoring

--- Tools.py
+++ Tools.py
@@ -39,10 +39,19 @@ def load_data():
     stazioni = gpd.GeoDataFrame(
         stazioni, geometry=gpd.points_from_xy(stazioni.longitude, stazioni.latitude)).set_crs('epsg:4326')
     # Loading geo_units and coastlines
-    geo_units = gpd.read_file('GeoUnits/shapefile/geo_units.shp')
-    coastline = gpd.read_file('coastline/add_coastline_medium_res_line_v7_4.shp')
+    geo_units = gpd.read_file('data/GeoUnits/shapefile/geo_units.shp')
+    unconsolidated_classes = ['Hs', 'Qk', 'Quc', 'Qc', 'Qu', 'Qa', 'Qs']
+    topography_classes = ['w', 'water', 'ice', '?', 'unknown']
 
-    return rock_cropout, mean_elev, stazioni, geo_units, coastline
+    unconsolidated = geo_units[geo_units.MAPSYMBOL.isin(unconsolidated_classes)]
+    rocks = geo_units[
+        ~(geo_units.MAPSYMBOL.isin(unconsolidated_classes)) & ~(geo_units.MAPSYMBOL.isin(topography_classes))]
+
+    del geo_units
+
+    coastline = gpd.read_file('data/coastline/add_coastline_medium_res_line_v7_4.shp')
+
+    return rock_cropout, mean_elev, stazioni, unconsolidated, rocks, coastline
 
 
 def find_nearest_value(origin_df, lon, lat, var_name):
@@ -56,3 +65,52 @@ def find_nearest_value(origin_df, lon, lat, var_name):
 def get_elevation(rock_cropout, mean_elev):
     return pd.Series(rock_cropout.apply(lambda row: find_nearest_value(
         mean_elev, row.geometry.centroid.x, row.geometry.centroid.y, 'elevation'), axis=1))
+
+
+def get_basemap(projection='spstere', boundinglat=-60, lon_0=180, resolution='h'):
+    m = Basemap(projection=projection, boundinglat=boundinglat, lon_0=lon_0, resolution=resolution)
+    return m
+
+
+def get_basemap_projection(geometry, basemap=None):
+    if not basemap:
+        basemap = get_basemap()
+    if not geometry.crs:
+        "CRS non definito"
+        return None
+    geometry = geometry.to_crs("EPSG:3031")
+    centr_x = geometry.centroid.x
+    centr_y = geometry.centroid.y
+    centroids = gpd.GeoDataFrame(geometry=gpd.points_from_xy(centr_x, centr_y)) \
+        .set_crs('epsg:3031').to_crs('epsg:4326')
+    points_x, points_y = basemap(centroids.geometry.x, centroids.geometry.y)
+    return pd.DataFrame({'x': points_x, 'y': points_y})
+
+
+def viz_init():
+    m = get_basemap()
+    plt.figure(figsize=(15, 15))
+    m.drawcoastlines()
+    m.fillcontinents(color='white', lake_color='aqua')
+    m.drawmapboundary(fill_color='lightblue')
+    return m
+
+
+def get_cluster_data(data):
+    data = data.to_crs("epsg:3031")
+    cluster_data = pd.DataFrame({'lon': data.geometry.centroid.x,
+                                 'lat': data.geometry.centroid.y,
+                                 'area': data.geometry.area,
+                                 'elev': data.elevation})
+    cluster_data = gpd.GeoDataFrame(
+        cluster_data, geometry=gpd.points_from_xy(cluster_data.lon, cluster_data.lat)).set_crs('epsg:3031')
+    cluster_data = cluster_data.drop(columns=['lon', 'lat'])
+    cluster_data['x'] = cluster_data.geometry.apply(lambda point: point.x)
+    cluster_data['y'] = cluster_data.geometry.apply(lambda point: point.y)
+    return cluster_data
+
+
+def scale_data(data):
+    scaler = MinMaxScaler()
+    data = data.drop(columns=['geometry'])
+    return scaler.fit_transform(data)
